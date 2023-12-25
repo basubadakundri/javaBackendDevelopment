8347d3de5376e9486388ff21c6b0026b252d38fe
Basavaraj <basubadakundri@gmail.com>
2023-12-25 19:55:54 +0530

    some update

---
+++ ProgrammingPractice/src/inputOutput/AsappJobsClass4.java
@@ -0,0 +1,13 @@
+package inputOutput;
+
+public class AsappJobsClass4 {
+	public static void main(String[] args) {
+
+		short s = 0;
+		int a = 07;
+//		int b = 08; any number start with 0 is treated as octal number.
+//		so this reason 08 give compiler error octal number is from 0-7
+		int c = 112345;
+
+	}
+}
